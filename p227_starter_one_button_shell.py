3dc9ff5f60e283d434a4538310457e706c56a19b
williamengh-rgb <William.Engh@crpusd.org>
2026-01-12 09:30:54 -0800

    added a comment

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -1,10 +1,7 @@
-import subprocess
+import subprocess # setup for button 
 import tkinter as tk
 import tkinter.scrolledtext as tksc
-from tkinter import filedialog
-from tkinter.filedialog import asksaveasfilename
-
-def do_command():
+from 
     command = ["ping", "localhost"]
     # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
