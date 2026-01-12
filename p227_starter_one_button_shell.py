fd002ff76605dfbcc6bf1cfd40e398099e9812ce
williamengh-rgb <William.Engh@crpusd.org>
2026-01-12 09:35:35 -0800

    added 1st block of code

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -7,7 +7,7 @@ from
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
     
     subprocess.run(command)
-    
+  
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
@@ -17,3 +17,22 @@ ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
 
 root.mainloop()
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
